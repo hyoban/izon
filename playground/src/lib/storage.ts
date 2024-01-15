6fd4fbb79ac833f1ca28da62e94684e6a8990884
Stephen Zhou <hi@hyoban.cc>
2024-01-16 00:19:13 +0800

    chore: try Cloudflare KV

--- playground/src/lib/storage.ts
+++ playground/src/lib/storage.ts
@@ -1,10 +1,20 @@
 import { createStorage } from "unstorage"
-import redisDriver from "unstorage/drivers/redis"
+import cloudflareKVHTTPDriver from "unstorage/drivers/cloudflare-kv-http"
+
+// import redisDriver from "unstorage/drivers/redis"
+
+// export const kv = createStorage({
+//   driver: redisDriver({
+//     host: process.env.REDIS_HOST,
+//     port: Number(process.env.REDIS_PORT),
+//     password: process.env.REDIS_PASSWORD,
+//   }),
+// })
 
 export const kv = createStorage({
-  driver: redisDriver({
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    password: process.env.REDIS_PASSWORD,
+  driver: cloudflareKVHTTPDriver({
+    accountId: process.env.CF_ACCOUNT_ID!,
+    namespaceId: process.env.CF_NAMESPACE_ID!,
+    apiToken: process.env.CF_API_TOKEN!,
   }),
 })
