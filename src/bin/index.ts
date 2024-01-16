dc4ff49bd5b7ea71bb21f523f8b9bdba50770d4e
Stephen Zhou <hi@hyoban.cc>
2024-01-16 15:55:15 +0800

    fix: make cli work

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,27 +1,35 @@
+#!/usr/bin/env node
 import { hideBin } from "yargs/helpers"
 import yargs from "yargs/yargs"
 
 import { getDependents } from ".."
 
-const argv = await yargs(hideBin(process.argv))
-  .option("limit", {
-    alias: "l",
-    type: "number",
-    default: 10,
-  })
-  .option("maxPage", {
-    alias: "m",
-    type: "number",
-    default: 10,
-  })
-  .parse()
+async function main() {
+  const argv = await yargs(hideBin(process.argv))
+    .option("limit", {
+      alias: "l",
+      type: "number",
+      default: 10,
+    })
+    .option("maxPage", {
+      alias: "m",
+      type: "number",
+      default: 10,
+    })
+    .parse()
 
-const targets = argv._.map(String)
+  const targets = argv._.map(String)
 
-for (const target of targets) {
-  const { result } = await getDependents(target, {
-    limit: argv.limit,
-    maxPage: argv.maxPage,
-  })
-  console.table(result)
+  for (const target of targets) {
+    const { result } = await getDependents(target, {
+      limit: argv.limit,
+      maxPage: argv.maxPage,
+    })
+    console.table(result)
+  }
 }
+
+main().catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
